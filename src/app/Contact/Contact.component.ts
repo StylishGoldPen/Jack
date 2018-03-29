863cc7fa781ece41769f686d3da1f5123c222526
StylishGoldPen <stylishgoldpen@yahoo.com.sg>
2018-03-30 01:44:56 +0800

    initial push

---
+++ src/app/Contact/Contact.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './Contact.component.html',
+  styleUrls: ['./Contact.component.css']
+})
+
+export class ContactComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent} from './Home/Home.component';
+import { ProjectsComponent } from './Projects/Projects.component';
+import { AboutComponent } from './About/About.component';
+import { ContactComponent } from './Contact/Contact.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
+    NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
